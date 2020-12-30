3e49dcbb0c7a05c654926effbbd652845fa25f78
Vladislavkn <inova136@gmail.com>
2020-12-30 23:32:34 +0300

    add logger

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'DEVELOPMENT_MODE_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'sqlite:///{os.path.join(basedir, "app.db")}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+
