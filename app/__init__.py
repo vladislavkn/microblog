600f09a1a4b14dd52bdf01dfba5be82ac285a014
Vladislavkn <inova136@gmail.com>
2021-01-03 18:06:49 +0300

    add send_email function

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from config import Config
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
 
 login.login_view = 'login'
 
--- config.py
+++ config.py
@@ -11,6 +11,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['vladislav.kns@gmail.com']
     POSTS_PER_PAGE = 12
-
