1ac3cfbfa10b781f307b21537566f53274434cf6
Vladislavkn <inova136@gmail.com>
2021-01-05 11:50:54 +0300

    add bootstrap styles

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 login.login_view = 'login'
 
